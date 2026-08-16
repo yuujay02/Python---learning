ac8f0ee2dbd9cc8237f02782bc51924daf87eb09
yuujay02 <ujjwalkukreti0205@gmail.com>
2026-08-16 11:49:54 +0530

    Restore report card printer script

---
+++ project-01/report_card_printer.py
@@ -0,0 +1,11 @@
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
